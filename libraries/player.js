97b0e0ed10bde21a35f45343b150c044e62474a8
ayush713 <75534884+ayush713@users.noreply.github.com>
2021-05-02 22:10:46 +0530

    Add files via upload

---
+++ libraries/player.js
@@ -0,0 +1,9 @@
+class player{
+    constructor()  {
+        this.index=null;
+        this.dstance=0;
+        this.name=null;
+        this.score=0;
+        
+    }
+}
---
+++ player.js
@@ -0,0 +1,8 @@
+class player{
+    constructor(){
+        this.index=null;
+        this.distance=0;
+        this.name=null;
+        this.score=0;
+    }
+}
